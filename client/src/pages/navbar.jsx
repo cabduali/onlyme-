dfe558c3192e6365b5773952450757324558db5c
Abubakar-Ciise <abubakarciise4@gmail.com>
2024-07-25 14:30:46 +0300

    first commit

--- client/src/pages/navbar.jsx
+++ client/src/pages/navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Social
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
